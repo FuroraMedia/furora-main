fd56f82297968382b658a13c4c958695d05d97ca
Graeme Chidgey <graeme.chidgey@gmail.com>
2017-02-10 07:26:06 +0000

    fix ssl header issue

--- server/util/ssl.js
+++ server/util/ssl.js
@@ -2,9 +2,10 @@ const forceSSL = (req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       res.redirect(['https://', req.get('Host'), req.url].join(''));
+    } else {
+      next();
     }
   }
-  next();
 };
 
 export default forceSSL;
