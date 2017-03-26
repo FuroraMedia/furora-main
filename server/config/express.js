96e7bd20c6c68098c898a48a75fae6f8a7e26d1e
Graeme Chidgey <graeme.chidgey@gmail.com>
2017-03-26 21:34:02 +0100

    https

--- server/config/express.js
+++ server/config/express.js
@@ -17,7 +17,7 @@ const expressConfig = (app) => {
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  if (process.env.HTTPS_REDIRECT === true) {
+  if (process.env.HTTPS_REDIRECT) {
     app.use(forceSSL);
   }
   app.use('/static', express.static(path.join(__dirname, '../../client/dist'), {
